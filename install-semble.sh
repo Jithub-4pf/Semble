73f91b1aa1cb6534d8717c4c7ec7f5f4f85c704b
dunderstab <mymcalt12345@gmail.com>
2024-06-20 14:20:33 -0700

    beta 1.0.1 testing 2

--- install-semble.sh
+++ install-semble.sh
@@ -2,5 +2,8 @@ echo "Installing Semblelang!"
 git clone https://github.com/Jithub-4pf/Semble.git
 mkdir ~/.semble
 cp -rf Semble ~/.semble
-cp Semble/semble.sh semble.sh 
 rm -rf Semble
+g++ -o semble ~~/.semble/Semble/main.cpp
+sudo chmod 775 ~/.semble/Semble/semble
+export PATH=~/.semble/Semble:$PATH
+echo "done!"
